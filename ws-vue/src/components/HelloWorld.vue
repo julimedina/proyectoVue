7f976e888bbbe1ce00efe43adc9f2d9411653381
Angel <angel.peraza@peaku.io>
2024-03-05 21:19:14 -0500

    prueba

--- ws-vue/src/components/HelloWorld.vue
+++ ws-vue/src/components/HelloWorld.vue
@@ -5,31 +5,31 @@
         <div class="justify-content-center align-items-center col-md-1 col-3">
           <img src="favicon.ico" alt="" width="70" height="70" class="rounded"><img>
         </div>
-        <div class="col-md-11 col-9" v-for="trabajo in trabajos" :key="trabajo.id" >
+        <div class="col-md-11 col-9" v-for="(trabajo, index) in trabajos" :key="index" >
           <div class="row align-items-center">
             <div class="mb-2 col" >
               <a href="#" class="text-dark stretched-link d-inline d-md-inline-flex">
-                <h2 class="bold pl-md-2 pr-md-2 mb-0 pb-0">{{ trabajos.titleVacante}}</h2>
-                <section class="pl-md-2 d-md-inline-flex align-items-center text-wrap" >
+                <h2 class="bold pl-md-2 pr-md-2 mb-0 pb-0">{{ trabajo.titleVacante }}</h2>
+                <section class="pl-md-2 d-md-inline-flex align-items-center text-wrap">
                   <div class="text-truncated border dark text-dark text-capitalize" style="font-size: 0.8em; margin-bottom: 0px !important;">
-                    <span></span>
+                    <span>{{ trabajo.titleVacante }}</span>
                   </div>
                   <div class="text-truncated border dark text-dark text-capitalize" style="font-size: 0.8em; margin-bottom: 0px !important;">
-                    <span></span>
+                    <span>{{trabajo.titleVacante}}</span>
                   </div>
                   <div class="text-truncated border dark text-dark text-capitalize" style="font-size: 0.8em; margin-bottom: 0px !important;">
-                    <span></span>
+                    <span>{{trabajo.titleVacante}}</span>
                   </div>
                 </section>
               </a>
             </div>
           </div>
           <div class="row pl-md-2 mt-1 flex-column flex-md-row" style="font-size: 0.9em;">
-            <div class="text-truncated text-capitalize col"></div>            
-            <div class="text-truncated text-capitalize col"></div>            
-            <div class="text-truncated text-capitalize col"></div>            
-            <div class="text-truncated text-capitalize col"></div>            
-            <div class="text-truncated text-capitalize col"></div>            
+            <div class="text-truncated text-capitalize col">{{trabajo.titleVacante}}</div>            
+            <div class="text-truncated text-capitalize col">{{trabajo.titleVacante}}</div>            
+            <div class="text-truncated text-capitalize col">{{trabajo.titleVacante}}</div>            
+            <div class="text-truncated text-capitalize col">{{trabajo.titleVacante}}</div>            
+            <div class="text-truncated text-capitalize col">{{trabajo.titleVacante}}</div>            
           </div> 
         </div>   
       </div>            
@@ -48,17 +48,40 @@ export default {
   }, 
   data(){
     return {
-      trabajos: {
+      trabajos: [{
         id: 1,
         titleVacante: 'Analista de infraestructura',
         epresa: 'Expert Tech',
-        skill: ['HTML', 'CSS','JS'],
+        skills: ['HTML', 'CSS','JS'],
+        ciudad: "Medellín",
+        salario: "1.9M - 2.1M COP",
+        vacante: "Vacantes 1",
+        about: "14 hours ago",
+        publicacion: "publicacion numero 1"
+      },
+      {
+        id: 2,
+        titleVacante: 'Analista de infraestructura',
+        epresa: 'Expert Tech',
+        skills: ['HTML', 'CSS','JS'],
+        ciudad: "Medellín",
+        salario: "1.9M - 2.1M COP",
+        vacante: "Vacantes 1",
+        about: "14 hours ago",
+        publicacion: "publicacion numero 1"
+      },
+      {
+        id: 2,
+        titleVacante: 'Analista de infraestructura',
+        epresa: 'Expert Tech',
+        skills: ['HTML', 'CSS','JS'],
         ciudad: "Medellín",
         salario: "1.9M - 2.1M COP",
         vacante: "Vacantes 1",
         about: "14 hours ago",
         publicacion: "publicacion numero 1"
       }
+    ]
     }
   },
     methods: {  
