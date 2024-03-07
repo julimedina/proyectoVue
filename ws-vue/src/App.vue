083ff79f877b16821cd6c528c25080f2b9cdffab
Angel <angel.peraza@peaku.io>
2024-03-06 23:18:59 -0500

    Se agrego las img y se puso las propiedades

--- ws-vue/src/App.vue
+++ ws-vue/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">    
+    <!-- <img alt="Vue logo" src="./assets/logo.png">     -->
     <div class="card-body p-2">
       <HelloWorld/>
     </div>
--- ws-vue/src/components/HelloWorld.vue
+++ ws-vue/src/components/HelloWorld.vue
@@ -1,35 +1,29 @@
 <template> 
   
     <div class="card-body p-2 p-md-3" style="background-color: rgb(175, 175, 207);"> 
-      <div class="row align-items-center">
+      <div class="row align-items-center" v-for="(trabajo, index) in trabajos" :key="index">
         <div class="justify-content-center align-items-center col-md-1 col-3">
           <img src="favicon.ico" alt="" width="70" height="70" class="rounded"><img>
         </div>
-        <div class="col-md-11 col-9" v-for="(trabajo, index) in trabajos" :key="index" >
+        <div class="col-md-11 col-9" >
           <div class="row align-items-center">
             <div class="mb-2 col" >
-              <a href="#" class="text-dark stretched-link d-inline d-md-inline-flex">
+              <a href="#" class="text-dark stretched-link d-inline d-md-inline-flex" style="text-decoration: none;">
                 <h2 class="bold pl-md-2 pr-md-2 mb-0 pb-0">{{ trabajo.titleVacante }}</h2>
                 <section class="pl-md-2 d-md-inline-flex align-items-center text-wrap">
-                  <div class="text-truncated border dark text-dark text-capitalize" style="font-size: 0.8em; margin-bottom: 0px !important;">
-                    <span>{{ trabajo.titleVacante }}</span>
-                  </div>
-                  <div class="text-truncated border dark text-dark text-capitalize" style="font-size: 0.8em; margin-bottom: 0px !important;">
-                    <span>{{trabajo.titleVacante}}</span>
-                  </div>
-                  <div class="text-truncated border dark text-dark text-capitalize" style="font-size: 0.8em; margin-bottom: 0px !important;">
-                    <span>{{trabajo.titleVacante}}</span>
-                  </div>
+                  <div v-for="(skill, index) in trabajo.skills" :key="index" class="text-truncated border dark text-dark text-capitalize" style="font-size: 0.8em; margin-bottom: 0px !important;">
+                    <span>{{ skill }}</span>
+                  </div>                  
                 </section>
               </a>
             </div>
           </div>
           <div class="row pl-md-2 mt-1 flex-column flex-md-row" style="font-size: 0.9em;">
-            <div class="text-truncated text-capitalize col">{{trabajo.titleVacante}}</div>            
-            <div class="text-truncated text-capitalize col">{{trabajo.titleVacante}}</div>            
-            <div class="text-truncated text-capitalize col">{{trabajo.titleVacante}}</div>            
-            <div class="text-truncated text-capitalize col">{{trabajo.titleVacante}}</div>            
-            <div class="text-truncated text-capitalize col">{{trabajo.titleVacante}}</div>            
+            <div class="text-truncated text-capitalize col">{{trabajo.ciudad}}</div>            
+            <div class="text-truncated text-capitalize col">{{trabajo.salario}}</div>            
+            <div class="text-truncated text-capitalize col">{{trabajo.vacante}}</div>            
+            <div class="text-truncated text-capitalize col">{{trabajo.about}}</div>            
+            <div class="text-truncated text-capitalize col">{{trabajo.publicacion}}</div>            
           </div> 
         </div>   
       </div>            
@@ -52,12 +46,12 @@ export default {
         id: 1,
         titleVacante: 'Analista de infraestructura',
         epresa: 'Expert Tech',
-        skills: ['HTML', 'CSS','JS'],
+        skills: ['HTML', 'CSS','JS', 'Node'],
         ciudad: "Medellín",
         salario: "1.9M - 2.1M COP",
         vacante: "Vacantes 1",
-        about: "14 hours ago",
-        publicacion: "publicacion numero 1"
+        about: "24 hours ago",
+        publicacion: "publicacion numero 0"
       },
       {
         id: 2,
@@ -67,11 +61,11 @@ export default {
         ciudad: "Medellín",
         salario: "1.9M - 2.1M COP",
         vacante: "Vacantes 1",
-        about: "14 hours ago",
+        about: "15 hours ago",
         publicacion: "publicacion numero 1"
       },
       {
-        id: 2,
+        id: 3,
         titleVacante: 'Analista de infraestructura',
         epresa: 'Expert Tech',
         skills: ['HTML', 'CSS','JS'],
@@ -79,7 +73,7 @@ export default {
         salario: "1.9M - 2.1M COP",
         vacante: "Vacantes 1",
         about: "14 hours ago",
-        publicacion: "publicacion numero 1"
+        publicacion: "publicacion numero 2"
       }
     ]
     }
