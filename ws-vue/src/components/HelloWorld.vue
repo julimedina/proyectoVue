46a2c47dfa3c6594f64e4de94016717c615b011a
julimedina <juli.mm134@gmail.com>
2024-06-21 16:15:41 -0500

    subiendo proyecto

--- ws-vue/src/components/HelloWorld.vue
+++ ws-vue/src/components/HelloWorld.vue
@@ -1,14 +1,16 @@
 <template>   
-    <div class="container cont" > 
+    <div class="container" > 
       <div class="componentes card-body p-3" v-for="(trabajo, index) in trabajos" :key="index">
-        <div class="justify-content-center align-items-center col-md-1 col-3">
-          <img :src="rutaImagen" alt="" width="70" height="70" class="rounded">
-        </div>      
+             
         <div class="col-md-11 col-9 ">
+          
             <div class="row align-items-center">
+              <div class="justify-content-center align-items-center col-md-1 col-3">
+          <img :src="rutaImagen" alt="" width="70" height="70" class="rounded">
+        </div> 
               <div class="mb-2 col" >
                 <a href="#" class="text-dark stretched-link d-inline d-md-inline-flex" style="text-decoration: none;">
-                  <h2 class="bold pl-md-2 pr-md-2 mb-0 pb-0">{{ trabajo.titleVacante }}</h2>
+                  <strong><h4 class="bold pl-md-2 pr-md-2 mb-0 pb-0">{{ trabajo.titleVacante }}</h4></strong>
                   <section class="pl-md-2 d-md-inline-flex align-items-center text-wrap">                    
                     <div class="text-truncated border dark text-dark text-capitalize m-1" style="font-size: 0.8em; margin-bottom: 0px !important;" v-for="(skill, index) in trabajo.skills" :key="index">
                       <span> {{ skill }}</span>
@@ -28,6 +30,7 @@
         </div>            
       </div>
     </div>
+    
 
         
 
@@ -114,7 +117,6 @@ export default {
     border: solid 1px rgb(189, 188, 188);
     border-radius: 5px; 
     margin: 1rem;  
-    z-index: 3;
   position: relative;
     
 }
